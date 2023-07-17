419c4264b61bfe08e287663eacfe1b8c5ffbd724
Pearl Nguyen <57478320+lalastitch@users.noreply.github.com>
2023-07-17 10:54:59 -0700

    Weather Dashboard functionality and basic styling
    If there is time, make the dashboard more interactive and better looking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TipCalulatorComponent } from './tip-calulator/tip-calulator.component';
 import { GroceryListComponent } from './grocery-list/grocery-list.component';
 import { RPSComponent } from './rps/rps.component';
-import { WeatherComponent } from './weather/weather.component';
+import { WeatherDashboardComponent } from './weather/weather.component';
 import { CustomTipComponent } from './custom-tip/custom-tip.component';
 import { ResultDialogComponent } from './result-dialog/result-dialog.component';
 
@@ -18,7 +20,7 @@ import { ResultDialogComponent } from './result-dialog/result-dialog.component';
     TipCalulatorComponent,
     GroceryListComponent,
     RPSComponent,
-    WeatherComponent,
+    WeatherDashboardComponent,
     CustomTipComponent,
     ResultDialogComponent
   ],
@@ -26,7 +28,9 @@ import { ResultDialogComponent } from './result-dialog/result-dialog.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    MatDialogModule
+    MatDialogModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { WeatherDashService } from '../weatherDash.service';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css']
 })
-export class WeatherComponent {
+export class WeatherDashboardComponent {
+  cityName: string = '';
+  weatherData: any;
 
+  constructor(private WeatherDashService: WeatherDashService) {}
+
+  searchWeather() {
+    if (this.cityName) {
+      this.WeatherDashService.getWeatherByCity(this.cityName).subscribe((data: any) => {
+        this.weatherData = data;
+      });
+    }
+  }
 }
