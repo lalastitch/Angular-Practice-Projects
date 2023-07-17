6c23036fc811801a2df86896a1a2c3f1f86a9871
Pearl Nguyen <57478320+lalastitch@users.noreply.github.com>
2023-07-17 06:35:13 -0700

    Completed styling and functionality of RPS

--- src/app/data/dialog-info.ts
+++ src/app/data/dialog-info.ts
@@ -1,4 +1,5 @@
 export interface DialogInfo {
     result:string,
-    url:string
+    url:string,
+    sent:string
 }
--- src/app/rps/rps.component.ts
+++ src/app/rps/rps.component.ts
@@ -14,6 +14,7 @@ export class RPSComponent {
   info:DialogInfo = {
     result:"",
     url:"",
+    sent:""
   }
 
   player(choice:string){
@@ -22,37 +23,44 @@ export class RPSComponent {
     if (choice.localeCompare(compChoice) == 0){ // tie
 
       this.info.result = "You Tied";
-      this.info.url = ".\resultImages\tie.jpg";
+      this.info.url = "resultImages/tie.jpg";
+      this.info.sent = "You both chose the same thing!";
 
     } else if (choice.localeCompare("rock") == 0 && compChoice.localeCompare("paper") == 0){ // player rock lost
 
       this.info.result = "You Lost!";
-      this.info.url = ".\resultImages\rockLoses.jpg";
+      this.info.url = "resultImages/rockLoses.jpg";
+      this.info.sent = "Your Dwayne Johnson Rock lost to paper";
 
     }  else if (choice.localeCompare("rock") == 0 && compChoice.localeCompare("scissor") == 0){ //  player rock won
 
-      this.info.url = ".\resultImages\scissorsLosing.jpg";
+      this.info.url = "resultImages/scissorsLosing.jpg";
       this.info.result = "You Won!";
+      this.info.sent = "Dwayne Johnson Rock tore the scissors like paper";
 
     } else if (choice.localeCompare("scissor") == 0 && compChoice.localeCompare("rock") == 0){ // player scissor lost
 
       this.info.result = "You Lost!";
-      this.info.url = ".\resultImages\scissorsLosing.jpg";
+      this.info.url = "resultImages/scissorsLosing.jpg";
+      this.info.sent = "Your scissors got smashed by Dwayne Johnson Rock";
 
     }  else if (choice.localeCompare("scissor") == 0 && compChoice.localeCompare("paper") == 0){ //  player scissor won
 
-      this.info.url = ".\resultImages\paperLoses.jpg";
+      this.info.url = "resultImages/paperLoses.jpg";
       this.info.result = "You Won!";
+      this.info.sent = "Your scissors cut up that paper easily";
 
     }  else if (choice.localeCompare("paper") == 0 && compChoice.localeCompare("scissor") == 0){ // player paper lost
 
       this.info.result = "You Lost!";
-      this.info.url = ".\resultImages\paperLoses.jpg";
+      this.info.url = "resultImages/paperLoses.jpg";
+      this.info.sent = "Your paper got shredded by scissors";
 
     }  else if (choice.localeCompare("paper") == 0 && compChoice.localeCompare("rock") == 0){ //  player paper won
 
       this.info.result = "You Won!";
-      this.info.url = ".\resultImages\rockLoses.jpg";
+      this.info.url = "resultImages/rockLoses.jpg";
+      this.info.sent = "Your paper smothered Dwayne Johnson Rock";
 
     }  
     this.openResultDialog(this.info);
